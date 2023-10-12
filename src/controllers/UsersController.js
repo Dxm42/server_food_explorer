b0893632f03b076ec6b70870089eea1e7dba1a3c
Dxm42 <lsantana1234@hotmail.com>
2023-10-12 12:36:25 -0300

    Add cadastro de usuarios no database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,22 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
+
 
 class UserController {
-  create(request, response){
+ async create(request, response){
     const { name, email, password, isAdmin } = request.body
 
-    if(!name){
-      throw new AppError("Nome é obrigatorio")
+    const database = await sqliteConnection()
+    
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso")
     }
 
+    await database.run("INSERT INTO users(name, email, password, isAdmin) VALUES (?, ?, ?, ?)",[name, email, password, isAdmin])
 
-    response.status(201).json({name, email, password})
+    return response.status(201).json()
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRuns = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 const express = require("express")
 
 const routes = require("./routes")
+migrationsRuns()
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
 
-database()
 app.use((error, request, response , next) => {
   if(error instanceof AppError){
     return response.status(error.statusCode).json({
